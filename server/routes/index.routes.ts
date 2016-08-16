89cf34b781dd5ad046d5e02413d3199be2bd8b9b
Nitish Dayal <nitishdayal@outlook.com>
2016-08-15 18:54:55 -0700

    commit

--- server/routes/index.routes.ts
+++ server/routes/index.routes.ts
@@ -6,23 +6,11 @@ const router: Router = Router()
 const users = new UserLogic()
 
 router.route( '/' )
-
-<<<<<<< HEAD
-    user.save()
-      .then(() => {
-        let userPromise = User.find().exec()
-
-        userPromise.then(( user ) => {
-          res.send( user )
-        })
-      })
-=======
   .post(( req: Request, res: Response ) => {
     let userName: string = req.body.name
     users.createNewUser( userName ).then(() => {
       users.getAllUsers.then(( users ) => res.send( users ))
     })
->>>>>>> 23e84e8c32e8bba1fb0ffab81fbf050ad08814d9
   })
 
   .get(( req: Request, res: Response ) => {
